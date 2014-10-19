11ce0b40bb7cc55ff1dddc858993eea5bddecc02
Alek Hurst <123alekhurst@gmail.com>
2014-10-18 21:20:43 -0700

    fuckit

--- controllers/feedbat_controller.js
+++ controllers/feedbat_controller.js
@@ -1,16 +1,17 @@
 var FeedbatApp = angular.module('FeedbatApp', []);
 
 FeedbatApp.controller('FeedbatController', function ($scope) {
-	$scope.view_title = "feedbaat",
+	$scope.view_title = "feedbat",
+	$scope.titles = ['feedbat', 'Me'],
 
-	$scope.aTestFunction = function(i) {
-		alert('called');
-	}
-
-	$scope.openCommentView= function(){
-		
-		$("#comment_view").removeClass('hide-sub-view').addClass("animated bounceInUp")
-	}
+	$scope.openCommentView = function(){	
+		$("#comment_view").removeClass('hide-sub-view animated bounceOutDown').addClass("animated bounceInUp")
+	},
+	
+	$scope.closeCommentView = function() {
+		$("#comment_view").removeClass('animated bounceInUp').addClass("animated bounceOutDown")
+	},
+	
 	$scope.openTipView = function(){
 		$("#tip_view").removeClass('hide-sub-view').addClass("animated bounceInUp")
 	}
