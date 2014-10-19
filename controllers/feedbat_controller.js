7265dc6c7e7f6c48f956b0203f03425badd3edca
scott-m-sarsfield <scott.m.sarsfield@gmail.com>
2014-10-19 00:28:51 -0700

    Swipe Proof of Concept

--- controllers/feedbat_controller.js
+++ controllers/feedbat_controller.js
@@ -15,8 +15,54 @@ FeedbatApp.controller('FeedbatController', function ($scope) {
 	$scope.openTipView = function(){
 		$("#tip_view").removeClass('hide-sub-view').addClass("animated bounceInUp")
 	}
+	
 });
 
 $(document).ready(function () {
+	
+});
+
+/* scott's crap */
+function Swipeable(el){
+	var start_coords = {};
+	var self = this;
+	
+	
+    var isTouch = 'ontouchstart' in window;
+	
+	var start = (isTouch) ? 'touchstart' : 'mousedown';
+	var end = (isTouch) ? 'touchend' : 'mouseup';
+	
+	this.getCoords = function(e){
+		if(!(e instanceof TouchEvent)) return e; // for mouse stuff.
+        
+		data = e.touches && e.touches.length ? e.touches : e.changedTouches;
+		return {
+        x: data[0].pageX,
+        y: data[0].pageY
+      };
+	}
+	
+	this._onStart = function(e){
+		e = self.getCoords(e);
+		start_coords.x = e.x;
+		el.addEventListener(end,self._onEnd,false);
+		console.log("hi")
+	};
+	
+	this._onEnd = function(e){
+		e = self.getCoords(e);
+		el.removeEventListener(end,self._onEnd);
+		if(e.x - start_coords.x > 100) $(el).addClass('animated fadeOutRight');
+		else if(e.x - start_coords.x < -100) $(el).addClass('animated fadeOutLeft');
+		console.log("bye");
+	};
+	
+	el.addEventListener(start,this._onStart,false);
+		
+}
+
+$(document).ready(function () {
+var x = new Swipeable($("#current-feedbat")[0]);
 	
 });
